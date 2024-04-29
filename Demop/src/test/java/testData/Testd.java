125e552820c1cc42af9e1e6d06a3ada9957028c8
aniru <aniru@LAPTOP-2G3SQ2CA>
2024-04-29 13:20:35 +0530

    Sec commit

--- Demop/src/test/java/testData/Testd.java
+++ Demop/src/test/java/testData/Testd.java
@@ -1,9 +1,13 @@
 package testData;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Testd {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.com");
 	}
 
 }
